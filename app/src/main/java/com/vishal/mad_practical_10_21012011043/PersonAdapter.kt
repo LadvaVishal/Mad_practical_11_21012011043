6db788b1c1d9ada43c997c1c1c98de3944dd0e40
ladav <ladavavishal22@gmail,com>
2023-10-26 18:59:07 +0530

    Initial commit

---
+++ app/src/main/java/com/vishal/mad_practical_10_21012011043/PersonAdapter.kt
@@ -0,0 +1,15 @@
+package com.vishal.mad_practical_10_21012011043
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+
+class PersonAdapter(val context: Context,val personArray:ArrayList<Person>):ArrayAdapter<Person>(context,0,personArray)
+{
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view=LayoutInflater.from(context).inflate(R.layout.person_item,parent,false)
+        return view
+    }
+}
