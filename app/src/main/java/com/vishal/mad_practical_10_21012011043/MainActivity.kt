e39e448889264f6dab33bd32dcc15eaf6964ae8f
ladav <ladavavishal22@gmail,com>
2023-10-27 19:35:41 +0530

    commit 2

--- app/src/main/java/com/vishal/mad_practical_10_21012011043/MainActivity.kt
+++ app/src/main/java/com/vishal/mad_practical_10_21012011043/MainActivity.kt
@@ -2,10 +2,76 @@ package com.vishal.mad_practical_10_21012011043
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fab: FloatingActionButton = findViewById(R.id.swapBtn)
+        fab.setOnClickListener {
+
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    val data = HttpRequest().makeServiceCall(
+                        "https://api.json-generator.com/templates/qjeKFdjkXCdK/data",
+                        "dchj8v1b6qqdjzbqood1jgpachyfzlw58r540gru")
+                    withContext(Dispatchers.Main) {
+                        try {
+                            if(data != null)
+                                runOnUiThread{getPersonDetailsFromJson(data)}
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
     }
+
+    private fun getPersonDetailsFromJson(sJson: String) {
+        val personList = ArrayList<Person>()
+        try {
+
+            val jsonArray = JSONArray(sJson)
+            for (i in 0 until jsonArray.length()) {
+                val jsonObject = jsonArray[i] as JSONObject
+                val person = Person(jsonObject)
+                personList.add(person)
+            }
+            val personadapter=findViewById<ListView>(R.id.listView1)
+            personadapter.adapter = PersonAdapter(this, personList)
+        } catch (ee: JSONException) {
+            ee.printStackTrace()
+        }
+
+    }
+
+    fun setArrayToListView(){
+
+
+        val personlistview=findViewById<ListView>(R.id.listView1)
+        val Array = arrayListOf<Person>(
+            Person("1","vishal","ladavavishal22@gmail.com","9081897150","Veraval",90.90,909.90),
+            Person("2","harshal","harshal21@gmail.com","9081897151","Rajkot",90.90,909.90),
+            Person("3","jay","jay22@gmail.com","9081897152","Mehasana",90.90,909.90),
+            Person("4","vivek","vivek@gmail.com","9081897153","Morbi",90.90,909.90),
+            Person("5","jvk","jvk@gmail.com","9725240046","surat",90.90,909.90)
+        )
+        personlistview.adapter=PersonAdapter(this,Array)
+
+
+    }
+
+   
 }
--- app/src/main/java/com/vishal/mad_practical_10_21012011043/Person.kt
+++ app/src/main/java/com/vishal/mad_practical_10_21012011043/Person.kt
@@ -1,6 +1,9 @@
 package com.vishal.mad_practical_10_21012011043
 
+import android.location.Address
+import org.json.JSONObject
 import java.io.Serializable
+import java.util.jar.Attributes
 
 class Person ( var id:String,
                var name:String,
@@ -10,4 +13,15 @@ class Person ( var id:String,
                var latitude:Double,
                var longitude:Double
 ):Serializable{
+    constructor(jsonObject:JSONObject):this("","","","","",0.0,8.0) {
+        id = jsonObject.getString("id")
+        emailId = jsonObject.getString("email")
+        phoneNo = jsonObject.getString("phone")
+        val profileJson = jsonObject.getJSONObject("profile")
+        name = profileJson.getString("name")
+        address = profileJson.getString("address")
+        val locationJson = profileJson.getJSONObject("location")
+        latitude = locationJson.getDouble("lat")
+        longitude = locationJson.getDouble("long")
+    }
 }
